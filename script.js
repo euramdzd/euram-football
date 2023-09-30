d2375305888bb9910a74e06bdad88a6f0e0b6d6c
Euram Dz <145721055+euramdzd@users.noreply.github.com>
2023-09-30 21:33:23 +0100

    script.js

--- script.js
+++ script.js
@@ -1,93 +1,117 @@
 
 let APIkey = "54005a89d767a637e08895f2008dd5764d00e3d6f4c36822f451ae2186e09f87";
 
-// 1 - دخول في موقع https://allsportsapi.com/
-
-// 2 - انشاء حساب و أخد key خاص بنتائج كرة قدم 
-
-// 3 - شهر فقط من استخدام مجاني لهدا   API
-
-
-// من أجل معرفة تاريخ اليوم
-
 var today = new Date();
 
 var Dat_e = ` ${today.getDate()} / ${today.getMonth()+1} / ${today.getFullYear()} `
 
 document.querySelector('.date span').innerHTML = Dat_e;
 
+
+
+// setInterval(()=>{ 
 let d = ` ${today.getFullYear()}-${today.getMonth()+1}-${today.getDate()} `
 
-// جلب قائمة مباريات اليوم
 
-  
+let url = 'https://apiv2.allsportsapi.com/football/?met=Fixtures&APIkey='+APIkey+'&timezone=Africa/Algiers&from='+d+'&to='+d
 
-fetch('https://apiv2.allsportsapi.com/football/?met=Fixtures&APIkey='+APIkey+'&timezone=Africa/Algiers&from='+d+'&to='+d)
+fetch(url)
 .then(res=>res.json())
 .then(res=>{
   let match = res.result;
-  let country = match.filter(i => i.country_name != "Israel" ).map(i => i.country_name) // تحديد دول مقام فيها مباريات
-  // ارجاع  دول بدون تكرار
+  if(!location.search){
+    let country = match.filter(i => i.country_name != "Israel" && i.event_country_key!= null ).map(i => i.country_name) // تحديد دول مقام فيها مباريات
     country = [...new Set(country)]
-   country.sort();
-  country = country.map(i => `<option value="${i}">${i}</option>`).join('')
 
-  document.getElementById('country').innerHTML = country
-  let m  = ""
-  for(let i=0;i<match.length;i++){
-    m  += `<div class="box" data-country="${match[i].country_name}" data-id="${match[i].event_key}">
-              <p>${match[i].event_status ? match[i].event_status : match[i].event_time}</p>
-              <p><img src="${match[i].away_team_logo}" class="logo" alt="-"> ${match[i].event_away_team}</p>
-              <p style="direction: rtl;">${match[i].event_final_result}</p>
-              <p><img src="${match[i].home_team_logo}" class="logo" alt="-">${match[i].event_home_team}</p>
-          </div>`
+
+    country.forEach((i,index)=>{
+      match.forEach(j=>{
+        if(j.country_name == i && j.event_country_key != null ){
+          country[index] = [i,j.country_logo,j.event_country_key]
+        }
+      })
+    })
+    console.log(country)
+
+    country = country.map((i) => `<a href="?id=${i[0]}" class="box-country">
+                                    <img src="${i[1]?i[1]:'https://apiv2.allsportsapi.com/logo/logo_country/'+i[2]+'_'+i[0].toLowerCase().replaceAll(' ','-')+'.png'}"/>
+                                    <span>${i[0]}</span>
+                                  </a>`).join('')
+
+    document.querySelector('.match').innerHTML = country;}
+    
+else{
+
+  if(location.search.split('=')[0] == "?id"){
+
+    let name = location.search.split('=')[1].replaceAll('%20'," ") 
+    document.querySelector('.nav').classList.add("act")
+    document.querySelector('.nav').innerHTML = `
+          <div class="container">
+          <a href="index.html">  قائمة الدول</a>
+          <span> / </span>
+          <a href="?id=${name}"> ${name} </a>
+          </div>
+          `
+    let league = match.filter(i => i.country_name != "Israel" && i.country_name == name).map(i => i.league_name) // تحديد دول مقام فيها مباريات
+    
+    let matchCountry = match.filter(i => i.country_name != "Israel" && i.country_name == name).map(i => i) // تحديد دول مقام فيها مباريات
+
+    league = [...new Set(league)]
+
+    // league.sort();
+
+    let arrM=[]
+    console.log(matchCountry)
+    for(let j = 0 ; j < league.length ; j++){
+
+      let m = '';
+
+      for(let i = 0 ; i < matchCountry.length  ; i++){
+
+        if(matchCountry[i].league_name == league[j])
+
+          m  += `<a class="box" href='?match=${matchCountry[i].event_key}' data-country="${matchCountry[i].country_name}" data-id="${matchCountry[i].event_key}">
+                  <p>${matchCountry[i].event_status ? matchCountry[i].event_status : matchCountry[i].event_time}</p>
+                  <p><img src="${matchCountry[i].away_team_logo}" class="logo" alt="-"> ${matchCountry[i].event_away_team}</p>
+                  <p style="direction: rtl;">${matchCountry[i].event_final_result}</p>
+                <p><img src="${matchCountry[i].home_team_logo}" class="logo" alt="-">${matchCountry[i].event_home_team}</p>
+        </a>`
+      } 
+      arrM[j] = `<h2 class="title">${league[j]}</h2><div>${m}</div>`;
+    }
+    document.querySelector('.match').innerHTML = arrM.join('');
+  
+  }else if(location.search.split('=')[0] == "?match"){
+
+    
+
+    let id = location.search.split('=')[1].replaceAll('%20'," ") 
+
+    let matchSelect = match.filter(i => i.country_name != "Israel" && i.event_key == id).map(i => i)[0]
+    console.log(matchSelect)
+    document.querySelector('.nav').classList.add("act") 
+    document.querySelector('.nav').innerHTML = `
+          <div class="container">
+          <a href="index.html">  قائمة الدول</a>
+          <span> / </span>
+          <a href="?id=${matchSelect.country_name}"> ${matchSelect.country_name} </a>
+          </div>
+          `
+    showDetails(matchSelect,match)
+
   }
-  document.querySelector('.match').innerHTML = m;
-  // console.log(match)
-
-let optionSelect = document.querySelector("#country");
-let arrBoxMatch = document.querySelectorAll(".match .box")
-
-if(localStorage.getItem('country')){
-  showMatch(localStorage.getItem('country') , arrBoxMatch)
-  optionSelect.value = localStorage.getItem('country') 
-
-}else{
-showMatch(optionSelect.value , arrBoxMatch)
-
 }
 
-optionSelect.onchange = () => {
-  showMatch(optionSelect.value , arrBoxMatch)
-  localStorage.setItem('country',optionSelect.value)
-}
-
-showDetails(arrBoxMatch ,match);
 
 }).catch(err =>console.log( "err"))
 
-function showMatch(country , arr){
-    arr.forEach(i=>{
-      i.classList.remove('hidd')
-      }
-    )
-    arr.forEach(i => {
-    if(country != i.getAttribute('data-country')){
-      i.classList.add("hidd")
-    }      
-    });
+// },500) 
 
-}
 
-function showDetails(arr , arrMatch){
-  arr.forEach(i=>{
-    i.onclick=()=>{
-      let match;
-      arrMatch.forEach(j=>{
-        if(j.event_key == (+i.getAttribute("data-id"))){
-          match = j;
-        }
-      })
+
+function showDetails(match , arrMatch){
+
 
       //احصائيات
       let statistics = match.statistics;
@@ -109,18 +133,26 @@ function showDetails(arr , arrMatch){
 
       let BallPossession  =  statistics.filter(i=>i.type=="Ball Possession")      .map(i => `<div><p>${i.away}</p><p>  الاستحواد</p><p>${i.home}</p></div>`).join('')
       //بطاقات
-      let boxCardsA = match.cards.filter(i => i.home_fault.length > 0 ).map(i=>`<p> ${i.time} <span>${i.home_fault}</span> <span class="${i.card}"></span></p>`)
-      let boxCardsB = match.cards.filter(i => i.home_fault.length <= 0 ).map(i=>`<p> ${i.time} <span>${i.away_fault}</span> <span class="${i.card}"></span></p>`)
+      let boxCardsA = match.cards.filter(i => i.home_fault.length > 0 ).map(i=>`<p > <span style="color:var(--main-color); padding-right:6px">${i.time}</span> <span>${i.home_fault}</span> <span class="${i.card}"></span></p>`)
+      let boxCardsB = match.cards.filter(i => i.home_fault.length <= 0 ).map(i=>`<p > <span style="color:var(--main-color); padding-right:6px">${i.time}</span> <span>${i.away_fault}</span> <span class="${i.card}"></span></p>`)
 
       // الاهداف
-      let boxGolA = match.goalscorers.filter(i=>i.home_scorer.length > 0).map(i=>`<p> ${i.time} <span>${i.home_scorer}</span>  <span style="background: white;width: 15px;height: 15px;display: inline-block;border-radius: 50%;"><img src="football.png"width="15" height="15"></span></p>`)
-      let boxGolB = match.goalscorers.filter(i=>i.home_scorer.length <= 0).map(i=>`<p> ${i.time} <span>${i.away_scorer}</span> <span style="background: white;width: 15px;height: 15px;display: inline-block;border-radius: 50%;"><img src="football.png" width="15" height="15"/> <span></p>`)
+      let boxGolA = match.goalscorers.filter(i=>i.home_scorer.length > 0).map(i=>`<p class="gool" > 
+                                                                                    <span>${i.time}</span> 
+                                                                                    <span>${i.home_scorer}</span>  
+                                                                                    <span>
+                                                                                      <img src="football.png">
+                                                                                    </span>
+                                                                                  </p>`)
+      let boxGolB = match.goalscorers.filter(i=>i.home_scorer.length <= 0).map(i=>`<p class="gool" >
+                                                                                    <span>${i.time}</span>
+                                                                                      <span>${i.away_scorer}</span> 
+                                                                                    <span><img src="football.png"> <span>
+                                                                                  </p>`)
 
-      document.querySelector('.container').innerHTML +=`
-      <div class="box-match">
+      document.querySelector('.match').innerHTML =`
+
         <div class="match-details">
-        <div class="close"><a href='index.html'>X</a></div>
-        <div style="    max-height: 500px;overflow-y: auto;  padding: 20px;  position: relative;        overflow-x: hidden;">
           <div class="header">
           <img src=${match.country_logo} class="country">
           ${match.league_logo?`<div style="background: var(--main-color);border-radius: 6px;margin: 55px 0 10px 0;display: flex;align-items: center;justify-content: center;"><img src="${match.league_logo}" class="leaguelogo"></div>` : ""}
@@ -158,9 +190,7 @@ function showDetails(arr , arrMatch){
           </div>
           </div>
           </div>
-        </div>
-      </div>
+
       `
     }
-  })
-}
+
